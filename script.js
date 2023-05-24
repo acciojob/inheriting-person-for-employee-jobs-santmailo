fa5b4da1cdd35665147f03678df4e63bc3333abc
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2023-05-24 17:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // complete this js code
 function Person(name, age) {
-	let greet = () => {
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
 }
 
-function Employee extends Person(name, age, jobTitle) {
-    super(name,age);
-    this.jobTitle = jobTitle;
-    let jobGreet = () => {
-        
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-    }
+Person.prototype.greet = () => {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.jobGreet = () => {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 // Do not change code below this line
