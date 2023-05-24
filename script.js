0054d5523e03402e0b2fb23322b0e718ee9fa3a2
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2023-05-24 17:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	let greet = () => {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee extends Person(name, age, jobTitle) {
+    super(name,age);
+    this.jobTitle = jobTitle;
+    let jobGreet = () => {
+        
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+    }
+}
 
 // Do not change code below this line
 window.Person = Person;
